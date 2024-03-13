a9fbd3b5aacd65db5ab4fb13c65e3ede36266ee6
suhyeonOh <sh020119@snu.ac.kr>
2024-03-13 18:03:18 +0900

    lecture1_virtualFunctionEx

---
+++ lectures/lecture1/virtualFunctionEx.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+
+
+class A {
+public:
+virtual void pr() {
+cout << "base" << endl;
+}
+};
+
+
+class B : public A {
+
+public:
+void pr() {
+cout << "derived" << endl;
+}
+
+};
+
+
+int main () {
+B b;
+A a = b; 
+// 복사 생성자가 호출된다. 복사 생성자에서는 b의 필드를 a로 복사하는데, b는 A의 유도 클래스인 B의 객체이므로 결국 스택 영역에 
+// 생성되는 것은 A타입의 객체이다.
+// what if A &a = b;  -> callByRef 이므로, A 타입의 a 객체가 가리키는 것은 B타입의 객체 b이다. 복사 하지 않는다.
+
+A *pA = &b;
+a.pr();
+pA->pr();
+
+return 0;
+}
--- polyMorphism/parametric/BasicParametric.cpp
+++ polyMorphism/parametric/BasicParametric.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 // 매개 변수 다형성 (컴파일 다형성)
 // 컴파일 시에 타입 인자에 따라 다른 함수를 생성하기 때문에 컴파일 다형적이다.
-// generic으로도 불린다.
 
 
 template <typename T>
@@ -13,7 +12,7 @@ T maximum(T a, T b) {
 // 컴파일러가 maximum이라는 함수 template을 이용하여 각 타입 인자에 맞게 적절한 함수를 generate 한다.
 
 
-// 임시 다형성 구현
+// 임시 다형성 구현(템플릿 특수화)
 template <> // 아래 구현은 템플릿 과 관련있음
 const char* maximum(const char * a, const char* b){
   string temp1(a);
